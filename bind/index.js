1fe034a68a60e991fb657286bd193a50ed32f8d5
YoM <33747055+Younger-man@users.noreply.github.com>
2023-12-31 22:05:31 +0800

    ✨ feat: apply

---
+++ bind/index.js
@@ -0,0 +1,20 @@
+/**
+ * bind 返回的是一个绑定上下文的函数
+ * 1.指定 this
+ * 2 传入参数
+ * 3 返回一个函数
+ * 4 柯里化
+ */
+
+Function.prototype.newBind = function (ctx) {
+  // 调用 bind 的不是函数，需要抛出异常
+  if (typeof this !== 'function') {
+    throw new Error('Function.prototype.bind - what is trying to be bound is not callable');
+  }
+  const fn = this;
+  const args = [...arguments].slice(1);
+  return function () {
+    const innerArgs = [...arguments];
+    return fn.apply(ctx, args.concat(innerArgs));
+  };
+};
